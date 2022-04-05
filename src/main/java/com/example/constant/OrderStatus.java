8b6f7c7f37480029177774fb81575439de48b344
JHY <human264@gmail.com>
2022-04-05 23:25:12 +0900

    ORG

---
+++ src/main/java/com/example/constant/OrderStatus.java
@@ -0,0 +1,6 @@
+package com.example.constant;
+
+public enum OrderStatus {
+    ORDER, CANCEL
+
+}
---
+++ src/test/java/com/example/service/MemberServiceTest.java
@@ -0,0 +1,145 @@
+package com.example.service;
+
+import com.example.dto.MemberFormDto;
+import com.example.entity.Member;
+import com.example.repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@TestPropertySource(locations = "classpath:application-test.properties")
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    public Member createMember() {
+        MemberFormDto memberFormDto = new MemberFormDto();
+        memberFormDto.setEmail("test@email.com");
+        memberFormDto.setName("홍길동");
+        memberFormDto.setAddress("서울시 마포구 합정동");
+        memberFormDto.setPassword("1234");
+
+        return Member.createMember(memberFormDto, passwordEncoder);
+    }
+
+    public Member createMember(String email, String password) {
+        MemberFormDto memberFormDto = new MemberFormDto();
+        memberFormDto.setEmail(email);
+        memberFormDto.setName("홍길동");
+        memberFormDto.setAddress("서울시 마포구 합정동");
+        memberFormDto.setPassword(password);
+
+        Member member = Member.createMember(memberFormDto, passwordEncoder);
+        return memberService.saveMember(member);
+    }
+
+    @Test
+    @DisplayName("화원가입 테스트")
+    public void saveMemberTest() {
+        Member member = createMember();
+        Member saveMember = memberService.saveMember(member);
+
+        assertEquals(member.getEmail(), saveMember.getEmail());
+        assertEquals(member.getName(), saveMember.getName());
+        assertEquals(member.getAddress(), saveMember.getAddress());
+        assertEquals(member.getPassword(), saveMember.getPassword());
+        assertEquals(member.getRole(), saveMember.getRole());
+    }
+
+    @Test
+    @DisplayName("중복 회원 가입 테스트")
+    public void saveDuplicateMemberTest() {
+        Member member1 = createMember();
+        Member member2 = createMember();
+        memberService.saveMember(member1);
+
+        Throwable e = assertThrows(IllegalStateException.class,  () -> {
+            memberService.saveMember(member2);});
+
+        assertEquals("이미 가입된 회원입니다.", e.getMessage());
+
+    }
+
+    @Test
+    @DisplayName("로그인 성공 테스트")
+    public void loginSuccessTest() throws Exception {
+        String email = "test@email.com";
+        String password = "123123";
+
+        this.createMember(email, password);
+
+        mockMvc.perform(formLogin().userParameter("email")
+                        .loginProcessingUrl("/members/login")
+                        .user(email)
+                        .password(password))
+                .andExpect(SecurityMockMvcResultMatchers.authenticated());
+
+    }
+
+
+    @Test
+    @DisplayName("로그인 실패 테스트")
+    public void loginFailtest() throws Exception {
+        String email = "test@email.com";
+        String password = "1234";
+
+        this.createMember(email, password);
+
+        mockMvc.perform(formLogin().userParameter("email")
+                        .loginProcessingUrl("/members/login")
+                        .user(email).password("12345"))
+                .andExpect(SecurityMockMvcResultMatchers.unauthenticated());
+    }
+
+
+    @Test
+    @DisplayName("Auditing 테스트")
+    @WithMockUser(username = "gildong", roles="USER")
+    public void auditingTest() {
+        Member newMember = new Member();
+        memberRepository.save(newMember);
+
+        em.flush();
+        em.clear();
+
+        Member member = memberRepository.findById(newMember.getId())
+                .orElseThrow(EntityNotFoundException::new);
+
+        System.out.println("register time : " + member.getRegTime());
+        System.out.println("update time : " + member.getUpdateTime());
+        System.out.println("created member : " + member.getCreatedBy());
+        System.out.println("modified member : " + member.getModifiedBy());
+    }
+}
